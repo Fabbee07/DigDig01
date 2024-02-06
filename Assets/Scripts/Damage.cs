dc745b25521a24811eab1e0e9fadded60acb9ffc
Johnny Ung <johnny.ung@elev.ga.lbs.se>
2024-02-06 08:58:57 +0100

    njkhgcxfgh

--- Assets/Scripts/Damage.cs
+++ Assets/Scripts/Damage.cs
@@ -21,12 +21,5 @@ public class NewBehaviourScript : MonoBehaviour
     {
 
     }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
 
-        if (collision.gameObject.tag == "Player")
-        {
-            playerHealth.TakeDamage(damage);
-        }
-    }
-}
+    public 
