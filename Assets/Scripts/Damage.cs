09093041668b0f21d35c541582bb583ef49e91cd
Fabian Lundberg <fabian.lundberg1@learnet.se>
2024-02-09 10:48:39 +0100

    right files2

--- Assets/Scripts/Damage.cs
+++ Assets/Scripts/Damage.cs
@@ -1,12 +1,12 @@
-using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
-public class Damage : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
-    public PlayerHealth playerHealth;
+    public Health playerHealth;
     public int damage = 2;
-    //public GameObject[] hearts;
+    public GameObject[] hearts;
     private int health;
 
     public int Health { get => health; set => health = value; }
@@ -21,14 +21,5 @@ public class Damage : MonoBehaviour
     {
 
     }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
 
-        if (collision.gameObject.tag == "Player")
-        {
-            Debug.Log("damage");
-            playerHealth.TakeDamage(damage);
-        }
-    }
 }
-
