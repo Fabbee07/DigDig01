4d2f366bc3b912cae3a6010ddf275ad30dc8c798
Fabian Lundberg <fabian.lundberg1@learnet.se>
2024-03-22 10:36:12 +0100

    Added music and fixed spawners

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,11 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
     public void LoadScene(string aSceneName)
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(aSceneName);
+        SceneManager.LoadScene(aSceneName);
     }
 }
