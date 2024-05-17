0a8a4c80ac6578a355156ac9891624e4eaeab082
Teo Prim <teo.prim@learnet.se>
2024-05-17 08:50:02 +0200

    kys

--- Assets/Scripts/EnemyScripts/Boss.cs
+++ Assets/Scripts/EnemyScripts/Boss.cs
@@ -2,17 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Boss : MonoBehaviour
+public class BossController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform player; // Reference to the player's transform
+    public float meleeRange = 5f; // The range within which the boss switches to melee
+    public float attackCooldown = 1f; // Cooldown time between attacks
 
-    // Update is called once per frame
+    private float lastAttackTime;
+
     void Update()
     {
-        
+        // Calculate the distance to the player
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+        // Check if the boss can attack
+        if (Time.time > lastAttackTime + attackCooldown)
+        {
+            if (distanceToPlayer <= meleeRange)
+            {
+                MeleeAttack();
+            }
+            else
+            {
+                RangedAttack();
+            }
+
+            lastAttackTime = Time.time;
+        }
+    }
+
+    void RangedAttack()
+    {
+        // Implement the ranged attack logic here
+        Debug.Log("Ranged Attack");
+    }
+
+    void MeleeAttack()
+    {
+        // Implement the melee attack logic here
+        Debug.Log("Melee Attack");
     }
 }
